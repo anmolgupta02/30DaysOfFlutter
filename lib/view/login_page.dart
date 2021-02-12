6a58a414239b40e0840de5f6b956309487945500
Anmol <anmolgupta02@gmail.com>
2021-02-13 04:02:36 +0530

    Day 5 - Added Routes, SingleChildScrollView & Navigator

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,56 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:thirtydaysofflutter/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 80,
-            ),
-            Image.asset("images/login_pic.png",
-              alignment: Alignment.topCenter,
-              fit: BoxFit.cover,
-            ),
-            Text(
-              "Welcome!",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold
+      child: SingleChildScrollView( //this can be used for making the UI a little Responsive if the height of phone is too small.
+        child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 80,
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "Username",
-                      hintText: "Please Enter your Username"
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: "Please Enter your Password"
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                      child: Text("Click Here To Login"),
-                    onPressed:  () {
-                      print("login was pressed! Bitch!");
-                    },
-                  )
-                ],
+              Image.asset("images/login_pic.png",
+                alignment: Alignment.topCenter,
+                fit: BoxFit.cover,
+                // height: 400,
               ),
-            ),
-          ],
-        )
+              Text(
+                "Welcome!",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Username",
+                        hintText: "Please Enter your Username"
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Please Enter your Password"
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    ElevatedButton(
+                        child: Text("Click Here To Login"),
+                      style: TextButton.styleFrom(
+                        minimumSize: Size(200, 50)
+                      ),
+                      onPressed:  () {
+                        print("login was pressed! Bitch!");
+                        Navigator.pushNamed(context, MyRoutes.homeRoute); //Used this to navigate to next page.
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+      )
       );
   }
 }

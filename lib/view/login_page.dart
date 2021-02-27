2ca3ec573f5cc1e83cb71d1dcc02e53acd553c7c
Anmol <anmolgupta02@gmail.com>
2021-02-28 04:50:13 +0530

    Added Input Validations

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -9,110 +9,131 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name = "";
   bool buttonChanged = false;
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Material(
         child: SingleChildScrollView(
       //this can be used for making the UI a little Responsive if the height of phone is too small.
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 80,
-          ),
-          Image.asset(
-            "images/login_pic.png",
-            alignment: Alignment.topCenter,
-            fit: BoxFit.cover,
-            // height: 400,
-          ),
-          Text(
-            "Welcome! $name",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Username",
-                      hintText: "Please Enter your Username"),
-                  onChanged: (value) {
-                    name = value;
-                    setState(() {});
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: "Please Enter your Password"),
-                  obscureText: true,
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Material(
-                  color: Colors.cyan,
-                  borderRadius:
-                  BorderRadius.circular(buttonChanged ? 70 : 8),
-
-                  child: InkWell(
-                    splashColor: Colors.white,
-                    onTap: () async {
-                      print("login was pressed! Bitch!");
-                      await Future.delayed(Duration(seconds: 1));
-                      Navigator.pushNamed(
-                          context,
-                          MyRoutes
-                              .homeRoute); //Used this to navigate to next page.
-                      setState(() {
-                        buttonChanged = true;
-                      });
-                    },
-                    child: AnimatedContainer(
-                      height: 50,
-                      width: buttonChanged ? 80 : 150,
-                      alignment: Alignment.center,
-                      child: buttonChanged
-                          ? Icon(
-                              Icons.check,
-                              color: Colors.white,
-                            )
-                          : Text(
-                              "Click here to login",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16),
-                            ),
-                      // decoration: BoxDecoration(
-                      //   color: Colors.cyan,
-                      //   // shape: buttonChanged
-                      //   //     ? BoxShape.circle
-                      //   //     : BoxShape
-                      //   //         .rectangle, //colors can be used either inside the decoration or on the main property
-                      //
-                      // ),
-                      duration: Duration(seconds: 1),
-                    ),
-                  ),
-                )
-                // ElevatedButton(
-                //     child: Text("Click Here To Login"),
-                //   style: TextButton.styleFrom(
-                //     minimumSize: Size(200, 50)
-                //   ),
-                //   onPressed:  () {
-                //     print("login was pressed! Bitch!");
-                //     Navigator.pushNamed(context, MyRoutes.homeRoute); //Used this to navigate to next page.
-                //   },
-                // )
-              ],
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 80,
             ),
-          ),
-        ],
+            Image.asset(
+              "images/login_pic.png",
+              alignment: Alignment.topCenter,
+              fit: BoxFit.cover,
+              // height: 400,
+            ),
+            Text(
+              "Welcome! $name",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Username",
+                        hintText: "Please Enter your Username"),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "username cannot be empty";
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "Please Enter your Password"),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Password cannot be empty";
+                      } else if (value.length < 6) {
+                        return "Password should be at least 6 characters";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Material(
+                    color: Colors.cyan,
+                    borderRadius: BorderRadius.circular(buttonChanged ? 70 : 8),
+                    child: InkWell(
+                      splashColor: Colors.white,
+                      onTap: () => movedToHome(context),
+                      child: AnimatedContainer(
+                        height: 50,
+                        width: buttonChanged ? 80 : 150,
+                        alignment: Alignment.center,
+                        child: buttonChanged
+                            ? Icon(
+                                Icons.check,
+                                color: Colors.white,
+                              )
+                            : Text(
+                                "Click here to login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
+                              ),
+                        // decoration: BoxDecoration(
+                        //   color: Colors.cyan,
+                        //   // shape: buttonChanged
+                        //   //     ? BoxShape.circle
+                        //   //     : BoxShape
+                        //   //         .rectangle, //colors can be used either inside the decoration or on the main property
+                        //
+                        // ),
+                        duration: Duration(seconds: 1),
+                      ),
+                    ),
+                  )
+                  // ElevatedButton(
+                  //     child: Text("Click Here To Login"),
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(200, 50)
+                  //   ),
+                  //   onPressed:  () {
+                  //     print("login was pressed! Bitch!");
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute); //Used this to navigate to next page.
+                  //   },
+                  // )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     ));
+  }
+
+  movedToHome(BuildContext context) async {
+    if (_formKey.currentState.validate()) {
+      setState(() {
+        buttonChanged = true;
+      });
+      print("login was pressed! Bitch!");
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(
+          context, MyRoutes.homeRoute); //Used this to navigate to next page.
+      setState(() {
+        buttonChanged = false;
+      });
+    }
   }
 }
